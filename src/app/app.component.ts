075f6d3d791c68fed5412083a5e554c9e7d0ecc5
CARREREOlivier <carrereolivier@yahoo.fr>
2024-06-04 10:59:04 +0200

    added footer and header placeholders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import {Component, OnInit} from '@angular/core';
 import {Router, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {TestFormComponent} from "./Forms/test-form/test-form.component";
 import {CommonModule} from "@angular/common";
+import {HeaderComponent} from "./Header/header/header.component";
+import {FooterComponent} from "./Footer/footer/footer.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,TestFormComponent, CommonModule, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, TestFormComponent, CommonModule, RouterLink, RouterLinkActive, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
