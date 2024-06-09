368d43f871e26ab430d24c3aa211a9c7df284e4b
CARREREOlivier <carrereolivier@yahoo.fr>
2024-06-09 08:40:29 +0200

    Connexion : login form created! call to  API to be created

--- src/app/Views/connexion/connexion.component.ts
+++ src/app/Views/connexion/connexion.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-connexion',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './connexion.component.html',
   styleUrl: './connexion.component.css'
 })
 export class ConnexionComponent {
+
+  loginForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  })
+
+  login() {
+    //CALL API with username and password
+    if (this.loginForm.invalid) {
+      return;
+    }
+    alert("Calling BackEnd to login")
+  }
 
 }
