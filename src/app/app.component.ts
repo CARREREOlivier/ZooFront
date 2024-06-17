7462ce74f26ecb9e54298670a62b7b69323769c8
CARREREOlivier <carrereolivier@yahoo.fr>
2024-06-17 12:59:32 +0200

    extracted landing page - accueil component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,14 @@ import {CommonModule} from "@angular/common";
 import {HeaderComponent} from "./Header/header/header.component";
 import {FooterComponent} from "./Footer/footer/footer.component";
 import {NavbarComponent} from "./Navigation/navbar/navbar.component";
+import {AccueilComponent} from "./Views/accueil/accueil.component";
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TestFormComponent, CommonModule, RouterLink, RouterLinkActive, HeaderComponent, FooterComponent, NavbarComponent,],
+  imports: [RouterOutlet, TestFormComponent, CommonModule, RouterLink, RouterLinkActive, HeaderComponent, FooterComponent, NavbarComponent, AccueilComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
