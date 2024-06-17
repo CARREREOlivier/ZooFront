d2c4b43ebff3b4c23cca93bd0ddc106851f47273
CARREREOlivier <carrereolivier@yahoo.fr>
2024-06-17 15:41:00 +0200

    accueil component amended

--- src/app/Views/accueil/accueil.component.ts
+++ src/app/Views/accueil/accueil.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import {ZooPresentationComponent} from "./zoo-presentation/zoo-presentation.component";
+import {AccueilServicesComponent} from "./accueil-services/accueil-services.component";
+import {AccueilHabitatsComponent} from "./accueil-habitats/accueil-habitats.component";
+import {AccueilAvisComponent} from "./accueil-avis/accueil-avis.component";
 
 @Component({
   selector: 'app-accueil',
   standalone: true,
-  imports: [],
+  imports: [
+    ZooPresentationComponent,
+    AccueilServicesComponent,
+    AccueilHabitatsComponent,
+    AccueilAvisComponent,
+  ],
   templateUrl: './accueil.component.html',
   styleUrl: './accueil.component.css'
 })
