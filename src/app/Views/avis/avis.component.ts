bdd5d5e8635f2dc773d309b31ab059fecf14b70d
CARREREOlivier <carrereolivier@yahoo.fr>
2024-06-21 16:36:43 +0200

    Avis form added.

--- src/app/Views/avis/avis.component.ts
+++ src/app/Views/avis/avis.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, NgForm} from "@angular/forms";
+import {RouterOutlet} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+
+
 
 @Component({
   selector: 'app-avis',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './avis.component.html',
   styleUrl: './avis.component.css'
 })
-export class AvisComponent {
+export class AvisComponent  implements OnInit {
+  constructor(private formBuilder: FormBuilder, private httpClient: HttpClient) { }
+  title = 'avis';
+  SERVER_URL = "http://localhost:8081/avis/";
 
+  onSubmit(form: NgForm) {
+    console.log('Your form data:', form.value);
+    this.httpClient.post(this.SERVER_URL, form.value).subscribe();
+  }
+  ngOnInit(): void {
+  }
 }
+
+
+
