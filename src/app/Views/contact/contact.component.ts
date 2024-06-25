867e87e9c6a83d433529df80d141a0df66ab72a7
CARREREOlivier <carrereolivier@yahoo.fr>
2024-06-25 14:10:58 +0200

    contact form made. Still some issues to resolve.

--- src/app/Views/contact/contact.component.ts
+++ src/app/Views/contact/contact.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {ContactFormComponent} from "../../Forms/contact-form/contact-form.component";
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    ContactFormComponent
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
