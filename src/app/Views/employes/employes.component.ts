26c9cb32882efc92cb810d720ad933891c4eda9f
CARREREOlivier <carrereolivier@yahoo.fr>
2024-06-26 20:41:56 +0200

    Espace-Employé view created and implemented.

--- src/app/Views/employes/employes.component.ts
+++ src/app/Views/employes/employes.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {NgForOf, NgIf} from "@angular/common";
+//import {environment} from "../../../environments/environment";
+
 
 @Component({
   selector: 'app-employes',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './employes.component.html',
   styleUrl: './employes.component.css'
 })
-export class EmployesComponent {
+export class EmployesComponent implements OnInit {
+  data: any[] = [];
+  tableHeaders: string[] = [];
+  constructor(private http: HttpClient) {}
 
+  ngOnInit() {
+    this.fetchData('zoo-services');
+  }
+  fetchData(endpoint: string) {
+    this.http.get<any[]>(`${"http://localhost:8081/"}${endpoint}/all`).subscribe(response => {
+      this.data = response;
+      if (response.length > 0) {
+        this.tableHeaders = Object.keys(response[0]);
+      } else {
+        this.tableHeaders = [];
+      }
+    });
+  }
 }
