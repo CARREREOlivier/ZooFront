b58fb2d63829e51d21d909d4852b8910251604ba
CARREREOlivier <carrereolivier@yahoo.fr>
2024-06-30 15:20:12 +0200

    character counter for textarea added

--- src/app/Views/avis/avis.component.ts
+++ src/app/Views/avis/avis.component.ts
@@ -1,29 +1,40 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, FormsModule, NgForm} from "@angular/forms";
-import {RouterOutlet} from "@angular/router";
-import {HttpClient} from "@angular/common/http";
-
-
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm } from "@angular/forms";
+import { RouterOutlet } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-avis',
   standalone: true,
   imports: [RouterOutlet, FormsModule],
   templateUrl: './avis.component.html',
-  styleUrl: './avis.component.css'
+  styleUrls: ['./avis.component.css']
 })
-export class AvisComponent  implements OnInit {
-  constructor(private formBuilder: FormBuilder, private httpClient: HttpClient) { }
-  title = 'avis';
-  SERVER_URL = "http://localhost:8081/avis/";
+export class AvisComponent implements OnInit {
+  SERVER_URL = "http://localhost:8081/avis/new";
+  remainingChars = 500; // Initialiser à 500 parce que 500 caractères dans la textarea "commentaire"
+  constructor(private httpClient: HttpClient) { }
 
   onSubmit(form: NgForm) {
-    console.log('Your form data:', form.value);
-    this.httpClient.post(this.SERVER_URL, form.value).subscribe();
-  }
-  ngOnInit(): void {
-  }
-}
+    // Ajouter "visible: false" aux données du formulaire
+    const formData = {
+      ...form.value,
+      visible: false
+    };
 
+    console.log('formulaire envoyé', formData);
+    this.httpClient.post(this.SERVER_URL, formData).subscribe(response => {
+      console.log('Response from server:', response);
+    }, error => {
+      console.error('Error:', error);
+    });
+  }
+
+  updateCharCount(event: Event) {
+    const inputElement = event.target as HTMLTextAreaElement;
+    this.remainingChars = 500 - inputElement.value.length;
+  }
+  ngOnInit(): void { }
+}
 
 
